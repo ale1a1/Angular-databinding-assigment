f27ac0ced9d8cf019ce5a5dd944d85523530be03
alessandro ladu <ale1a@libero.it>
2022-12-08 18:15:47 +0000

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { InstructionsComponent } from './instructions/instructions.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
-  declarations: [AppComponent, InstructionsComponent],
+  declarations: [AppComponent, InstructionsComponent, GameControlComponent, OddComponent, EvenComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
