3149a3c715f2eecb8d7ef69fcbb8ff459c0db8b6
alessandro ladu <ale1a@libero.it>
2022-12-08 18:06:49 +0000

    add assigment instructions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InstructionsComponent } from './instructions/instructions.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, InstructionsComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
